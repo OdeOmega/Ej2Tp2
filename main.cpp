7d4e132d587c9dbe1fb9f7f1e304ee4f9b5fb655
OdeOmega <agustinhermosillabaez@gmail.com>
2023-10-01 11:13:23 -0300

    commit

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 
 using ll = long long;
-const ll inf = 1e18;
+const ll inf = LLONG_MAX;
 
 pair<int,int> p,h;
 
 //vector<vector<vector<int>>> m;
 int filas, columnas;
 
-int buscar(pair<int,int> actual, int t, vector<vector<bool>> recorrido, pair<int,int> destino,vector<vector<int>> &bsas, vector<vector<vector<int>>> &m){
+ll buscar(pair<int,int> actual, ll t, vector<vector<bool>> recorrido, pair<int,int> destino,vector<vector<int>> &bsas, vector<vector<vector<ll>>> &m){
     if(m[t][actual.first][actual.second] != -1){ //Ya llegue alguna vez con el mismo tiempo
         return m[t][actual.first][actual.second];
     }
@@ -19,15 +19,18 @@ int buscar(pair<int,int> actual, int t, vector<vector<bool>> recorrido, pair<int
         if(actual == destino){ //Legué a destino, devuelvo el tiempo
             return t;
         }
-        if(t >= bsas[actual.first][actual.second] && bsas[actual.first][actual.second] != 0){ //Hay maifestación y llegué tarde
-            m[t][actual.first][actual.second] = inf;
-            return inf;
+        if(t >= bsas[actual.first][actual.second] && bsas[actual.first][actual.second] != 0){ //Hay manifestación y llegué tarde
+            //m[t][actual.first][actual.second] = inf;
+            ll INF = inf;
+            return INF;
         }
         if(actual.first < 0 || actual.second < 0 || actual.first >= filas || actual.second >= columnas){ //Me fui de rando de la matriz
-            return inf;
+            ll INF = inf;
+            return INF;
         }
         if(recorrido[actual.first][actual.second]){ //En esta mismo camino ya pasé por ahí
-            return inf;
+            ll INF = inf;
+            return INF;
         }
         else{
             recorrido[actual.first][actual.second] = true;
@@ -63,6 +66,7 @@ int buscar(pair<int,int> actual, int t, vector<vector<bool>> recorrido, pair<int
                 d = inf;
             }
             recorrido[actual.first][actual.second] = false;
+            m[t][actual.first][actual.second] = min(a,min(b,min(c,d)));
             return min(a,min(b,min(c,d)));
         }
     }
@@ -81,12 +85,22 @@ int main() {
             }
         }
         cin>> hospitalX >> hospitalY >> pacienteX >> pacienteY;
-        vector<vector<vector<int>>> m(filas*columnas,vector<vector<int>>(filas,vector<int>(columnas,-1)));
+        vector<vector<vector<ll>>> m(filas*columnas+1,vector<vector<ll>>(filas,vector<ll>(columnas,-1)));
+        vector<vector<vector<ll>>> mm(filas*columnas+1,vector<vector<ll>>(filas,vector<ll>(columnas,-1)));
         vector<vector<bool>> recorrido(filas,vector<bool>(columnas,false));
         pair<int,int> hospital = {hospitalX,hospitalY};
         pair<int,int> paciente = {pacienteX,pacienteY};
-        int aber = buscar(hospital,0,recorrido,paciente,bsas,m);
-        cout<< aber<<endl;
+        ll ida = buscar(hospital,0,recorrido,paciente,bsas,m);
+        ll vuelta = inf;
+        if(ida != inf){
+            vuelta = buscar(paciente,ida,recorrido,hospital,bsas,mm);
+        }
+        if(ida == inf || vuelta == inf){
+            cout << "IMPOSIBLE" << endl;
+        }
+        else{
+            cout<< ida << " " << vuelta << endl;
+        }
     }
     return 0;
 }
